a62be4846d630974d6fb4d8d3417025dfcb1da07
fkfkfj12 <fkfkfj12@naver.com>
2022-09-02 17:46:16 +0900

    123

---
+++ ch01/src/sec2/test2.java
@@ -0,0 +1,26 @@
+package sec2;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		int a=100, b=23, c;
+		float d, e, f;
+		c = a / b; 			//4 -> 자동 형 변환
+		System.out.println("c="+c);
+		d = a / b;			//4.0 -> 자동 형 변환
+		System.out.println("d="+d);
+		d = (float) a / b;
+		System.out.println("d="+d);
+		e = 500 / 3.0f;   	// .0f 붙여서 실수 연산
+		System.out.println("e="+e);
+		System.out.println("byte : "+Byte.MIN_VALUE+"~"+Byte.MAX_VALUE);
+		System.out.println("short :"+Short.MIN_VALUE+"~"+Short.MAX_VALUE);
+		System.out.println("int :"+Integer.MIN_VALUE+"~"+Integer.MAX_VALUE);
+		System.out.println("long :"+Long.MIN_VALUE+"~"+Long.MAX_VALUE);
+		System.out.println("float :"+Float.MIN_VALUE+"~"+Float.MAX_VALUE);
+		System.out.println("double :"+Double.MIN_VALUE+"~"+Double.MAX_VALUE);
+		
+	
+	}
+
+}
---
+++ ch01/src/sec3/print2.java
@@ -0,0 +1,12 @@
+package sec3;
+
+public class print2 {
+
+	public static void main(String[] args) {
+		int a=100;
+		float b=3.14f;
+		String c="kim";
+		
+	}
+
+}
